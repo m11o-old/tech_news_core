c619c933f3899d8ec710b86a876216c2fe235c06
大迫雅仁 <velonica2227@icloud.com>
2018-01-30 00:19:06 +0900

    feat: rspectを追加

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,21 @@ module TechNewsCore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = :ja
+    config.i18n.enforce_available_locales = true
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: true,
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
